7988660eb4d9df1f72a7dbcf9d455afd4a139453
Martin Ptáček <martin.ptace@gmail.com>
2023-06-04 10:03:06 +0200

    protobuf compiles

--- client/app/src/main/java/com/birdthedeveloper/prometheus/android/prometheus/android/exporter/worker/RemoteWriteSender.kt
+++ client/app/src/main/java/com/birdthedeveloper/prometheus/android/prometheus/android/exporter/worker/RemoteWriteSender.kt
@@ -1,11 +1,18 @@
 package com.birdthedeveloper.prometheus.android.prometheus.android.exporter.worker
 
+import remote.write.RemoteWrite.WriteRequest
+
 data class RemoteWriteConfiguration(
     val scrape_interval : Int,
     val remote_write_endpoint : String,
 )
 
-class RemoteWrite(config : RemoteWriteConfiguration) {
+class RemoteWriteSender(config : RemoteWriteConfiguration) {
 
     //TODO implement this thing
+
+    fun test(){
+        var request : WriteRequest
+    }
 }
+
